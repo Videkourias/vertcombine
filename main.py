19c56fddc9c8a3d935b77128f8a78f88e0ffa799
Dean <videkourias@gmail.com>
2020-09-13 14:33:45 -0400

    Added method to stitch together images

--- main.py
+++ main.py
@@ -2,41 +2,48 @@ import numpy as np
 import PIL, os
 from PIL import Image
 
-images = [] # Paths to image files
+images = []  # Paths to image files
 
 
 # Update the list of images using the 'directory' directory
 def getImages(directory):
+    images.clear()
     for root, dirs, files in os.walk(directory):
         for name in files:
             images.append(os.path.join(root, name))
 
 
-'''
-list_im = ['Test1.jpg', 'Test2.jpg', 'Test3.jpg']
-imgs    = [ PIL.Image.open(i) for i in list_im ]
-# pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
-min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
-imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+# Combine the images present in the images array
+def combineImages(name, method='v'):
+    imgs = [PIL.Image.open(i) for i in images]
 
-# save that beautiful picture
-imgs_comb = PIL.Image.fromarray( imgs_comb)
-imgs_comb.save( 'Trifecta.jpg' )
+    # Find image with smallest size and resize others to match it
+    min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
+    imgs_comb = np.hstack((np.asarray(i.resize(min_shape)) for i in imgs))
 
-# for a vertical stacking it is simple: use vstack
-imgs_comb = np.vstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
-imgs_comb = PIL.Image.fromarray( imgs_comb)
-imgs_comb.save( 'Trifecta_vertical.jpg' )
-'''
+    m = method.lower()
+
+    # Create image horizontal
+    if 'h' in m:
+        imgs_comb = PIL.Image.fromarray(imgs_comb)
+        imgs_comb.save('Complete/' + name + '.jpg')
+
+    # Create image vertical
+    if 'v' in m:
+        imgs_comb = np.vstack((np.asarray(i.resize(min_shape)) for i in imgs))
+        imgs_comb = PIL.Image.fromarray(imgs_comb)
+        imgs_comb.save('Complete/' + name + '_vertical.jpg')
 
 
-def main():
-    directory = 'Images'
+def main(name):
+    directory = 'Images/'
 
     # Update images array
     getImages(directory)
-    print(images)
+
+    # Combine the images
+    combineImages(name)
 
 
 if __name__ == '__main__':
-    main()
+    main(input('Provide image name: '))
